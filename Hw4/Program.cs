2eab5ca413c821875acc91891f297df88d259b45
MakarovDaniil <119426084+MakarovDaniil@users.noreply.github.com>
2022-12-09 16:18:00 +0300

    Add files via upload

---
+++ Hw4/Program.cs
@@ -0,0 +1,58 @@
+﻿// Вторая цифра числа
+
+// Console.WriteLine ("Введите число"); 
+// int number = Convert.ToInt32(Console.ReadLine());
+// while (number>99) 
+//     number = number / 10;
+// int secondnum = number % 10;
+// Console.WriteLine ("Вторая цифра числа - "+secondnum); 
+ 
+
+
+// Третья цифра числа
+
+// Console.WriteLine ("Введите число"); 
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 100)
+//     Console.WriteLine ("Нет третьей цифры");
+
+// while (number>999) 
+//     number = number / 10;
+// int secondnum = number;
+// if (number>99) 
+// {
+//     secondnum = number %100;
+//     secondnum = secondnum % 10;
+//     Console.WriteLine ("Третья цифра числа - "+secondnum); 
+// }
+
+
+
+// Является ли день недели выходным
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// {
+//     if (n == 6)
+//         Console.WriteLine (n+ "-й день недели это выходной"); 
+//     if (n == 7)
+//         Console.WriteLine (n+ "-й день недели это выходной"); 
+//     if (n > 7)
+//         Console.WriteLine ("нет такого дня недели"); 
+//     if (n < 1)
+//         Console.WriteLine ("нет такого дня недели"); 
+//         else if (n < 6)  
+//         Console.WriteLine (n+ "-й день недели это не выходной"); 
+// }
+
+
+//Является ли одно из чисел квадратом второго
+
+// Console.WriteLine ("Введите два числа"); 
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// if  (n2 == (n1 * n1))
+//     Console.WriteLine ("второе число явлиется квадратом первого"); 
+// if (n1 == (n2 *n2))
+//     Console.WriteLine ("первое число явлиется квадратом второго"); 
+// else if ((n2 != (n1 * n1)))
+//     Console.WriteLine ("ни одно из чисел не является квадратом другого");
